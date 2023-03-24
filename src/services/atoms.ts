e0137f423cd69fdaaa62b149275f3636114dae54
bengar-dev <benoitgar.pro@gmail.com>
2023-03-24 05:56:00 +0100

    [remove] unused function

--- src/services/atoms.ts
+++
@@ -1,8 +0,0 @@
-import { useRecoilState } from "recoil";
-import { toasterState } from "../atoms/ui";
-
-export const handleToggleToaster = (value: boolean) => {
-  const [toaster, setToaster] = useRecoilState(toasterState);
-
-  setToaster({ ...toaster, display: value });
-};
